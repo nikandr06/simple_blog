39ad452236e496b2646c50b339254bcc10aae0c7
dmitxe <XeDmitry@yandex.ru>
2013-08-08 16:50:29 +0700

    Добавлен функционал добавления новой статьи, и кнопка для редактирования, поставлен security на /blog/new/ и /blog/edit/

--- src/Dmitxe/SiteBundle/Menu/SiteMenu.php
+++ src/Dmitxe/SiteBundle/Menu/SiteMenu.php
@@ -3,8 +3,9 @@
 namespace Dmitxe\SiteBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
-class SiteMenu
+class SiteMenu extends ContainerAware
 {
     public function main(FactoryInterface $factory, array $options)
     {
@@ -17,7 +18,22 @@ class SiteMenu
         }
 
         $menu->addChild('Homepage',     ['route' => 'dmitxe_site_index']);
-        $menu->addChild('Blog',         ['route' => 'smart_blog_index']);
+        $title = 'Blog';
+        if (true === $this->container->get('security.context')->isGranted('ROLE_BLOGGER'))
+        {
+           $menu->addChild($title,         array('route' => 'smart_blog_index', 'attributes' => array('class' => 'dropdown')));
+           $menu[$title]->addChild(NULL, array('attributes' => array('class' => 'divider')));
+           $menu[$title]->setLinkAttributes(array('class' => 'dropdown-toggle', 'data-toggle' => 'dropdown', 'role'=>'button', 'id'=>'mmi_finance'));
+//        $menu[$title]->setLabel($title. ' <b class="caret"></b>')->setExtra('safe_label', TRUE);
+            $menu[$title]->setLabel($title, TRUE);
+            $menu[$title]->setChildrenAttributes(array('class' => 'dropdown-menu','role'=>'menu', 'aria-labelledby'=>'mmi_finance'));
+            $menu[$title]->addChild('Все записи', array('route' => 'smart_blog_index'));
+            $menu[$title]->addChild('Новая запись', array('route' => 'smart_blog_article_new'));
+        }
+        else
+        {
+            $menu->addChild($title,        ['route' => 'smart_blog_index']);
+        }
         $menu->addChild('Forum',        ['uri'   => 'http://forum.timedev.net/']);
 
         return $menu;
--- src/SmartCore/Bundle/BlogBundle/Controller/ArticleController.php
+++ src/SmartCore/Bundle/BlogBundle/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace SmartCore\Bundle\BlogBundle\Controller;
 
+use Dmitxe\BlogBundle\Entity\Article;
 use Pagerfanta\Exception\NotValidCurrentPageException;
 use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -74,4 +75,48 @@ class ArticleController extends Controller
             'form' => $form->createView(),
         ]);
     }
+
+    public function newAction(Request $request)
+    {
+        $article = new Article();
+//        $form = $this->createForm(new ServiceType(), $service);
+        $form = $this->createForm(new ArticleFormType(get_class($article)), $article);
+
+ /*       if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request, false);
+
+            if ($form->isValid()) {
+                // выполняем прочие действие, например, сохраняем задачу в базе данных
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($service);
+                $em->flush();
+                $this->get('session')->getFlashBag()->add('success','Новая услуга создана!');
+
+                return $this->redirect($this->generateUrl('monolith_cabinet_service'));
+            }
+        }*/
+        if ($request->isMethod('POST')) {
+            $form->submit($request);
+
+            if ($form->isValid()) {
+                $article = $form->getData();
+
+                /** @var \Doctrine\ORM\EntityManager $em */
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($article);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('smart_blog_article', ['slug' => $article->getSlug()] ));
+            }
+        }
+
+/*        return $this->render('MonolithAdminBundle:Service:new.html.twig', array(
+            'form' => $form->createView(),
+        ));*/
+        return $this->render('SmartBlogBundle::article_new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+
 }
